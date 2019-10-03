29133b8cf071ba102589ee073594a87b740c15f0
methias <tmn5@pm.me>
2019-10-03 17:33:56 +0300

    add create post functionality

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 
@@ -12,21 +12,19 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'gravylov3r'},
-            'body': 'A scorching day in the middle of Athens summer...'
-        },
-        {
-            'author': {'username': 'moustachemike420'},
-            'body': 'ήπιαμε καφέδες στην κυψέλη'
-        }
-    ]
-    return render_template('index.html', title="Sup!", posts=posts)
+    form  = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title="Sup!", form=form, posts=posts)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
