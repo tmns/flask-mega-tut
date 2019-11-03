c55acee36d19e64142f3a3fe051dd1f1b3e6ceeb
methias <tmn5@pm.me>
2019-11-03 15:37:47 +0200

    create auth token api routes

--- app/models.py
+++ app/models.py
@@ -1,15 +1,12 @@
 from flask import current_app, url_for
-from datetime import datetime
+from datetime import datetime, timedelta
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 from time import time
-import jwt
 from app.search import add_to_index, remove_from_index, query_index
-import json
-import redis
-import rq
+import jwt, json, redis, rq, os, base64
 
 
 class SearchableMixin(object):
@@ -109,6 +106,9 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
 
     tasks = db.relationship('Task', backref='user', lazy='dynamic')
 
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -198,6 +198,18 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
             if new_user and 'password' in data:
                 self.set_password(data['password'])
 
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -207,6 +219,12 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
             return
         return User.query.get(id)
 
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 @login.user_loader
 def load_user(id):
